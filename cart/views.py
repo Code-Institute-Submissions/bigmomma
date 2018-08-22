09d5d103094fddb6bbceb267c2d29bf36d1697f2
clivenoonan <clivenoonan93@gmail.com>
2018-08-22 13:24:44 +0000

    Adde template import

--- cart/views.py
+++ cart/views.py
@@ -4,7 +4,8 @@ from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 import stripe
 from django.conf import settings
-from order.models import Order, OrderItem
+from.template.loader import get_template
+from django.core.mail import EmailMessage
 
 
 def _cart_id(request):
