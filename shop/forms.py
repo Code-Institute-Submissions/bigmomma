84895400ea7993b8e1a8db501786c2a02ec6ed13
clivenoonan <clivenoonan93@gmail.com>
2018-09-11 23:37:37 +0000

    Fixed forms.py to save email apon order

--- shop/forms.py
+++ shop/forms.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, required=True)
     last_name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(max_length=254, help_text='eg. youremail@anyemail.com')
+    email = forms.EmailField(max_length=254, help_text='clivesdev@gmail.com')
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'password1', 'password2')
+        fields = ('first_name', 'last_name', 'email' 'username', 'password1', 'password2')
